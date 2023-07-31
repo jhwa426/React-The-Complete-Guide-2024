259da60456362f502d9739fe88eb6871b8b4597a
Jeff Hwang <wodud6359@gmail.com>
2023-08-01 09:59:07 +1200

    Section01

--- Section01/demo/src/index.js
+++ Section01/demo/src/index.js
@@ -7,7 +7,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
