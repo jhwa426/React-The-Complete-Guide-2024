b36a4895ea71558b5fa620cb3607044321ae81c9
Jeff Hwang <wodud6359@gmail.com>
2023-08-12 19:32:11 +1200

    Section05

--- Section05/react-state-and-working-with-events/src/components/Expenses.js
+++ Section05/react-state-and-working-with-events/src/components/Expenses.js
@@ -10,13 +10,38 @@ function Expenses(props) {
 
     const [filteredYear, setFilteredYear] = useState('2020');
 
+    // const [filterInfoText, setFilterInfoText] = useState("2019, 2021 & 2022"); // better to use one state (each statement)
+
+    let filterInfoText = "2019, 2021 & 2022";
+
+    if (filteredYear === "2019") {
+        filterInfoText = "2020, 2021 & 2022";
+    } else if (filteredYear === "2021") {
+        filterInfoText = "2019, 2020 & 2022";
+    } else {
+        filterInfoText = "2019, 2020 & 2021";
+    }
+
+
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
+
+        // if (selectedYear === "2019") {
+        //     setFilterInfoText("2020, 2021 & 2022");
+        // } else if (selectedYear === "2020") {
+        //     setFilterInfoText("2019, 2021 & 2022");
+        // } else if (selectedYear === "2021") {
+        //     setFilterInfoText("2019, 2020 & 2022");
+        // } else {
+        //     setFilterInfoText("2019, 2020 & 2021");
+        // }
     }
 
     return (
         <Card className='expenses'>
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+
+            <p>Data for years {filterInfoText} is hidden.</p>
 
             <ExpenseItem
                 title={props.items[0].title}
