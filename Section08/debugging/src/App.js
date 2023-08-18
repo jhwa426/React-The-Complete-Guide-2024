2cb665f816d710ed57dbcf9ceb4d15d6c63d9cbd
Jeff Hwang <wodud6359@gmail.com>
2023-08-18 14:40:14 +1200

    Section08

--- Section08/debugging/src/App.js
+++ Section08/debugging/src/App.js
@@ -5,46 +5,46 @@ import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
 import './App.css';
 
 const App = () => {
-  const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
-  ]);
+    const [courseGoals, setCourseGoals] = useState([
+        { text: 'Do all exercises!', id: 'g1' },
+        { text: 'Finish the course!', id: 'g2' }
+    ]);
 
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: 'goal1' });
-      return updatedGoals;
-    });
-  };
+    const addGoalHandler = enteredText => {
+        setCourseGoals(prevGoals => {
+            const updatedGoals = [...prevGoals];
+            updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+            return updatedGoals;
+        });
+    };
 
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
-      return updatedGoals;
-    });
-  };
+    const deleteItemHandler = goalId => {
+        setCourseGoals(prevGoals => {
+            const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+            return updatedGoals;
+        });
+    };
 
-  let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
-  );
-
-  if (courseGoals.length > 0) {
-    content = (
-      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    let content = (
+        <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
     );
-  }
 
-  return (
+    if (courseGoals.length > 0) {
+        content = (
+            <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+        );
+    }
 
-      <section id="goal-form">
-        <CourseInput onAddGoal={addGoalHandler} />
-      </section>
-      <section id="goals">
-        {content}
-      </section>
-
-  );
+    return (
+        <div>
+            <section id="goal-form">
+                <CourseInput onAddGoal={addGoalHandler} />
+            </section>
+            <section id="goals">
+                {content}
+            </section>
+        </div>
+    );
 };
 
 export default App;
--- Section08/debugging/src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ Section08/debugging/src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -3,15 +3,15 @@ import React from 'react';
 import './CourseGoalItem.css';
 
 const CourseGoalItem = (props) => {
-  const deleteHandler = () => {
-    props.onDelete(props.id);
-  };
+    const deleteHandler = () => {
+        props.onDelete(props.id);
+    };
 
-  return (
-    <li className="goal-item" onClick={deleteHandler}>
-      {props.children}
-    </li>
-  );
+    return (
+        <li className="goal-item" onClick={deleteHandler}>
+            {props.children}
+        </li>
+    );
 };
 
 export default CourseGoalItem;
