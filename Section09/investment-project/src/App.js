74f1543d4799616adec9743fe886c1c616691b57
Jeff Hwang <wodud6359@gmail.com>
2023-08-22 11:59:30 +1200

    Section09

--- Section09/investment-project/src/App.js
+++ Section09/investment-project/src/App.js
@@ -1,98 +1,49 @@
-import logo from "./assets/investment-calculator-logo.png";
+import Header from './components/Header/Header';
+import UserInput from './components/UserInput/UserInput';
+import ResultTable from './components/ResultTable/ResultTable';
+import { useState } from 'react';
 
 function App() {
-  const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
+
+    const [userInput, setUserInput] = useState(null);
+
+
+    const calculateHandler = (userInput) => {
+        setUserInput(userInput);
+    };
 
     const yearlyData = []; // per-year results
 
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
+    if (userInput) {
+        let currentSavings = +userInput["current-savings"];
+        const yearlyContribution = +userInput["yearly-contribution"];
+        const expectedReturn = +userInput["expected-return"] / 100;
+        const duration = +userInput["duration"];
 
-    // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution
-      });
+        // The below code calculates yearly results (total savings, interest etc)
+        for (let i = 0; i < duration; i++) {
+            const yearlyInterest = currentSavings * expectedReturn;
+            currentSavings += yearlyInterest + yearlyContribution;
+            yearlyData.push({
+                year: i + 1,
+                yearlyInterest: yearlyInterest,
+                savingsEndOfYear: currentSavings,
+                yearlyContribution: yearlyContribution
+            });
+        }
     }
 
-    // do something with yearlyData ...
-  };
+    return (
+        <div>
+            <Header />
 
-  return (
-    <div>
-      <header className="header">
-        <img src={logo} alt="logo" />
-        <h1>Investment Calculator</h1>
-      </header>
+            <UserInput onCalculate={calculateHandler} />
 
-      <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
+            {!userInput && <p style={{ textAlign: 'center' }}>No investment calculated yet.</p>}
+            {userInput && <ResultTable data={yearlyData} initialInvestment={userInput["current-savings"]} />}
+
         </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
-        </p>
-      </form>
-
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
-
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+    );
 }
 
 export default App;
