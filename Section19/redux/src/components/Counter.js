5517f32fa662604c4a765f29760cc2597123138b
Jeff Hwang <wodud6359@gmail.com>
2023-12-01 16:27:29 +1300

    Section19

--- Section19/redux/src/components/Counter.js
+++ Section19/redux/src/components/Counter.js
@@ -1,30 +1,38 @@
 import { Component } from "react";
 import classes from './Counter.module.css';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { INCREMENT } from "../store";
 
 
 const Counter = () => {
     const dispatch = useDispatch();
     const counter = useSelector(state => state.counter);
+    const show = useSelector((state) => state.showCounter);
 
     const incrementHandler = () => {
-        dispatch({ type: "increment" })
+        dispatch({ type: INCREMENT })
+        // dispatch({ type: "increment" })
     };
 
     const decrementHandler = () => {
         dispatch({ type: "decrement" })
     };
 
-    const toggleCounterHandler = () => {
+    const increaseHandler = () => {
+        dispatch({ type: "increase", amount: 5 })
+    }
 
+    const toggleCounterHandler = () => {
+        dispatch({ type: "reset" })
     };
 
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
-            <div className={classes.value}>{counter}</div>
+            {show && <div className={classes.value}>{counter}</div>}
             <div className="counter button">
                 <button onClick={incrementHandler}>Increment</button>
+                <button onClick={increaseHandler}>Increase by 5</button>
                 <button onClick={decrementHandler}>Decrement</button>
             </div>
             <button onClick={toggleCounterHandler}>Toggle Counter</button>
@@ -32,27 +40,46 @@ const Counter = () => {
     );
 };
 
-class Counter extends Component {
-    incrementHandler() { }
-
-    decrementHandler() { }
-
-    toggleCounterHandler() { }
-
-
-    render() {
-        return (
-            <main className={classes.counter}>
-                <h1>Redux Counter</h1>
-                <div className={classes.value}>{counter}</div>
-                <div className="counter button">
-                    <button onClick={incrementHandler}>Increment</button>
-                    <button onClick={decrementHandler}>Decrement</button>
-                </div>
-                <button onClick={toggleCounterHandler}>Toggle Counter</button>
-            </main>
-        );
-    }
-}
-
 export default Counter;
+
+// class Counter extends Component {
+//     incrementHandler() {
+//         this.props.increment();
+//     }
+
+//     decrementHandler() {
+//         this.props.decrement();
+//     }
+
+//     toggleCounterHandler() { }
+
+
+//     render() {
+//         return (
+//             <main className={classes.counter}>
+//                 <h1>Redux Counter</h1>
+//                 <div className={classes.value}>{this.props.counter}</div>
+//                 <div className="counter button">
+//                     <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//                     <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//                 </div>
+//                 <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//             </main>
+//         );
+//     }
+// }
+
+// const mapStateToProps = (state) => {
+//     return {
+//         counter: state.counter
+//     };
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         increment: () => dispatch({ type: "increment" }),
+//         decrement: () => dispatch({ type: "decrement" }),
+//     };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- Section19/redux/src/store/index.js
+++ Section19/redux/src/store/index.js
@@ -1,15 +1,38 @@
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === "increment") {
-        return {
+export const INCREMENT = "increment";
+
+const initialState = { counter: 0, showCounter: true }
+
+const counterReducer = (state = initialState, action) => {
+    if (action.type === INCREMENT) {
+        // state.counter++;
+
+        // return state;
+        return { // must be override 
             counter: state.counter + 1,
+            showCounter: state.showCounter,
         }
     }
 
     if (action.type === "decrement") {
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter,
+        }
+    }
+
+    if (action.type === "increase") {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter,
+        }
+    }
+
+    if (action.type === "reset") {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
 
