8da07c3249139acf05d30aef1d18b31941d7fbc7
Jeff Hwang <wodud6359@gmail.com>
2024-01-18 10:46:33 +1300

    Section25

--- Section25/nextjs-project/components/layout/Layout.js
+++ Section25/nextjs-project/components/layout/Layout.js
@@ -2,12 +2,12 @@ import MainNavigation from './MainNavigation';
 import classes from './Layout.module.css';
 
 function Layout(props) {
-  return (
-    <div>
-      <MainNavigation />
-      <main className={classes.main}>{props.children}</main>
-    </div>
-  );
+    return (
+        <div>
+            <MainNavigation />
+            <main className={classes.main}>{props.children}</main>
+        </div>
+    );
 }
 
 export default Layout;
---
+++ Section25/nextjs-project/pages/new-meetup/index.js
@@ -0,0 +1,2 @@
+// our-domain.com/new-meetup
+
