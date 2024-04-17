8ce58d0640b31a571cc24792e47ef69b51e30ca6
Jeff Hwang <wodud6359@gmail.com>
2024-04-18 11:11:05 +1200

    Section19

--- Section19/redux-revision/revision/src/components/Counter.js
+++ Section19/redux-revision/revision/src/components/Counter.js
@@ -4,7 +4,9 @@ import { Component } from "react";
 
 const Counter = () => {
     const dispatch = useDispatch();
+
     const counter = useSelector(state => state.counter);
+    const show = useSelector(state => state.showCounter);
 
     const incrementHandler = () => {
         dispatch({ type: "increment" });
@@ -22,12 +24,14 @@ const Counter = () => {
         dispatch({ type: "decrease", amount: 5 });
     }
 
-    const toggleCounterHandler = () => { };
+    const toggleCounterHandler = () => {
+        dispatch({ type: "toggle" });
+    };
 
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
-            <div className={classes.value}>{counter}</div>
+            {show && <div className={classes.value}>{counter}</div>}
             <div>
                 <button onClick={incrementHandler}>Increment</button>
                 <button onClick={increaseHandler}>Increase by 5</button>
--- Section19/redux-revision/revision/src/store/index.js
+++ Section19/redux-revision/revision/src/store/index.js
@@ -1,27 +1,40 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
     if (action.type === "increment") {
         return {
             counter: state.counter + 1,
+            showCounter: state.showCounter,
         };
     }
     // action payload
     if (action.type === "increase") {
         return {
             counter: state.counter + action.amount,
+            showCounter: state.showCounter,
         };
     }
 
     if (action.type === "decrement") {
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter,
         };
     }
 
     if (action.type === "decrease") {
         return {
             counter: state.counter - action.amount,
+            showCounter: state.showCounter,
+        }
+    }
+
+    if (action.type === "toggle") {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter,
         }
     }
 
