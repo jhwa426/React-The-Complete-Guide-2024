63dba2b2f4c184f586d7d3c99fc3830ff98f449c
Jeff Hwang <wodud6359@gmail.com>
2024-06-07 14:32:23 +1200

    Section20

--- Section20/redux-advanced-revision/revision/src/App.js
+++ Section20/redux-advanced-revision/revision/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout>
+            <Cart />
+            <Products />
+        </Layout>
+    );
 }
 
 export default App;
